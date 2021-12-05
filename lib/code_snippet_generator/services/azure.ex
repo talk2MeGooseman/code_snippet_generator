d9ca867de876a393e610e1039b09449e783ed224
Erik Guzman <erik.guzman@guzman.codes>
2021-12-05 23:31:58 +0000

    Redo cognitive module and reimplement OCR flow

--- lib/code_snippet_generator/services/azure.ex
+++ lib/code_snippet_generator/services/azure.ex
@@ -2,8 +2,45 @@ defmodule Azure do
   def api_client,
     do: Application.get_env(:code_snippet_generator, :azure_cognitive_client)
 
-    @spec read_analyze(String.t()) :: any()
-    def read_analyze(image_url) do
-      api_client().read_analyze(image_url)
+  @spec read_analyze(String.t()) :: any()
+  def read_analyze(image_url) do
+    with {:ok, response} <- api_client().post("/read/analyze", %{ url: image_url }),
+          202 <- Map.get(response, :status_code) do
+        response
+        |> Map.get(:headers)
+        |> Enum.find(fn ({ header, _value}) -> header == "Operation-Location" end)
+        |> then(fn ({ _header, value }) -> {:ok, value} end)
+    else
+      _ ->
+        {:error, "Failed to read image"}
     end
+  end
+
+  @spec read_results(String.t()) :: any()
+  def read_results(results_url) do
+    path = Azure.Cognitive.parse_operation_location(results_url)
+
+    with {:ok, response} <- api_client().get(path),
+          200 <- Map.get(response, :status_code) do
+
+      {:ok, Map.get(response, :body)}
+    else
+      _ ->
+        {:error, "Failed to read image"}
+    end
+  end
+
+  def analyze_and_read_results(image_url) do
+    with {:ok, operation_url} <- read_analyze(image_url),
+      {:ok, results} <- read_results(operation_url) do
+        # Check status notStarted: The operation has not started.
+        # running: The operation is being processed.
+        # failed: The operation has failed.
+        # succeeded: The operation has succeeded.
+        {:ok, results}
+    else
+      _ ->
+        {:error, "Failed to read image"}
+    end
+  end
 end
--- lib/code_snippet_generator/services/azure/cognitive.ex
+++ lib/code_snippet_generator/services/azure/cognitive.ex
@@ -1,17 +1,28 @@
 defmodule Azure.Cognitive do
-  import HttpBase
+  use HTTPoison.Base
 
-  @behaviour Azure.CognitiveProvider
+  def parse_operation_location(url) do
+    "https://westus.api.cognitive.microsoft.com/vision/v3.2" <> path = url
+    path
+  end
 
-  @impl Azure.CognitiveProvider
-  def read_analyze(target_image_url) do
-    headers = [
+  def process_request_url(url) do
+    "https://westus.api.cognitive.microsoft.com/vision/v3.2" <> url
+  end
+
+  def process_response_body("" = body), do: body
+
+  def process_response_body(body) do
+    Jason.decode!(body)
+  end
+
+  def process_request_headers(headers \\ []) do
+    headers ++ [
       {"Content-Type", "application/json"},
       {"Ocp-Apim-Subscription-Key", System.get_env("COGNITIVE_COMPUTER_VISION_SERVICE_KEY")}
     ]
-
-    body = %{url: target_image_url}
-
-    post("https://westus.api.cognitive.microsoft.com/vision/v3.2/read/analyze", headers, body)
   end
+
+  def process_request_body(body), do: Jason.encode!(body)
+
 end
